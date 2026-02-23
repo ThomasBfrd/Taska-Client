ea5425fd4f9573b46859c2754795711c951fa401
ThomasBfrd <thomas.bfrd@gmail.com>
2026-02-23 15:00:08 +0100

    feat: mise en place du postit draggable

---
+++ src/features/login/components/drag-logs/drag-logs.component.ts
@@ -0,0 +1,42 @@
+import { Component, computed, input, InputSignal, signal, Signal, WritableSignal } from '@angular/core';
+import {CdkDrag} from '@angular/cdk/drag-drop';
+import { LoginData } from '../../../../shares/interfaces/login-data.interface';
+
+export type ROLE = 'employee' | 'manager';
+
+@Component({
+  selector: 'app-drag-logs',
+  templateUrl: './drag-logs.component.html',
+  styleUrl: './drags-logs.components.css',
+  imports: [CdkDrag],
+})
+export class DragLogs {
+
+  public readonly accounts: InputSignal<Record<string, LoginData>> = input<Record<string, LoginData>>({});
+
+  protected readonly dragPosition: {x: number; y: number} = {x: 100, y: -30};
+
+  protected readonly checkedValue: WritableSignal<boolean> = signal<boolean>(false);
+
+  protected readonly selectedRole: Signal<ROLE> = computed(() => {
+    const checkedValue: boolean = this.checkedValue();
+
+    return !checkedValue ? 'employee' : 'manager';
+  });
+
+  protected readonly filteredAccounts: Signal<Record<string, LoginData>> = computed(() => {
+    const selectedRole: ROLE = this.selectedRole();
+    const accounts: Record<string, LoginData> = this.accounts();
+
+    return { [selectedRole]: accounts[selectedRole] };
+  })
+
+  protected Object: ObjectConstructor = Object;
+
+  protected toggleRole(event: Event): void {
+    const isChecked: boolean = (event.target as HTMLInputElement).checked;
+
+    this.checkedValue.set(isChecked);
+  }
+
+}
